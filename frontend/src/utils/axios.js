cd6975e0424efaddc195567051a7b6f3a7131050
ANAS KHAN <anaskhan2209k@gmail.com>
2026-04-25 14:34:24 +0530

    Instance fix for production

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'https://blog-app-i7i1.onrender.com/api/',
+    baseURL: 'https://blog-app-i7i1.onrender.com/api',
     withCredentials: true
 })
 
