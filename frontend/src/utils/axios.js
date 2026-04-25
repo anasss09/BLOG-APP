7520f3094ccffd9a6350d663f8e0b6eecb32afe4
ANAS KHAN <anaskhan@ANASs-MacBook-Air.local>
2026-04-25 14:14:27 +0530

    Instance for production

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:4000/api',
+    baseURL: 'https://blog-app-i7i1.onrender.com/api/',
     withCredentials: true
 })
 
